0c865c753145b1ce2af0d04d4823c4e8b2f786cf
hakim <hakiltheknight@gmail.com>
2024-06-13 18:13:43 +0100

    adding navbar and sidebar

---
+++ src/views/components/navbar.blade.php
@@ -0,0 +1,83 @@
+{{-- ID is a must for toggleing the collapse menu of the nav-bar
+EVENTS: "open-navbar" "close-navbar" "toggle-navbar" with ID of the navbar --}}
+{{-- there is no option for a custom nav only add supported classes --}}
+{{-- bgcolor, color varibales of class names it uses tailwind class as default  --}}
+{{-- clickOutside default is true --}}
+
+@props([
+    'id' => '',
+    'class' => '',
+    'bgColor' => 'bg-gray-100',
+    'color' => 'text-gray-700',
+    'clickOutside' => true
+])
+@php
+    $collapseId = uniqid(); 
+@endphp
+@push('scripts')
+    <script>
+        function navbarFn(id, collapseId)
+        {
+            return {
+                id: id,
+                collapseId: collapseId,
+                open(id)
+                {
+                    this.id == id ? $dispatch('open-collapse', {id: this.collapseId}) : '';
+                },
+                close(id)
+                {
+                    this.id == id ? $dispatch('close-collapse', {id: this.collapseId}) : '';
+                },
+                toggle(id)
+                {
+                    this.id == id ? $dispatch('toggle-collapse', {id: this.collapseId}) : '';
+                }
+            }
+        }    
+    </script>    
+@endpush
+
+
+<section x-data="navbarFn('{{$id}}', '{{$collapseId}}')">
+    <nav class="{{"flex justify-between items-center px-5 sm:px-10 md:px-20 h-[50px] md:h-[70px] $bgColor $color $class"}}"
+    id="{{$id}}"
+    x-on:open-navbar.window="open($event.detail.id)"
+    x-on:close-navbar.window="close($event.detail.id)"
+    x-on:toggle-navbar.window="toggle($event.detail.id)"
+    @if($clickOutside)
+    x-on:click.outside="$dispatch('close-collapse', {id: '{{ $collapseId }}'})"
+    @endif
+    {{ $attributes }}
+    >
+        <div class="max-h-[50px] md:max-h-[70px]">
+            {{ $brand }}
+        </div>
+        {{ $slot }}
+        <div>
+            <div class="hidden md:flex gap-4">
+                {{ $navMenu }}
+            </div>
+
+            <x-stm::button btn="icon" class="md:hidden" size="sm" x-on:click="$dispatch('toggle-collapse', {id: '{{ $collapseId }}'})">
+                <svg class="fill-gray-900 size-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                </svg>
+            </x-stm::button>
+        </div>
+    </nav>
+    <x-stm::collapse 
+        id="{{ $collapseId }}" 
+        :class="'px-5 sm:px-10 md:px-20 md:hidden'.' '.$bgColor.' '.$color">
+            <x-slot:btn>
+                <div></div>
+            </x-slot:btn>   
+        <div class="space-y-2">
+            @isset($collapseNavMenu)
+                {{ $collapseNavMenu }}
+            @else
+                {{ $navMenu }}
+            @endisset     
+        </div>  
+    </x-stm::collapse>
+</section>
---
+++ src/views/components/sidebar.blade.php
@@ -0,0 +1,160 @@
+{{-- ID is a must for toggleing the sidebar
+EVENTS: "open-sidebar" "close-sidebar" "toggle-sidebar" with ID of the sidebar --}}
+{{-- you can define the init state of sidebar with state var --}}
+{{-- you can choose defrent sidebars by choosing from : "standard", "expand", "custom" --}}
+{{-- by defining a class and transitionStart transitionEnd you can customize sidebar transition --}}
+{{-- you can choose the side for sidebar with position var "left" "right" --}}
+{{-- you can add or remove the breaking-point of the sidebar by the bp var : bool default "true"  --}}
+
+
+@props([
+    'id' => '',
+    'class' => 'bg-gray-100 fill-gray-700',
+    'state' => true,
+    'sidebar' => 'standard',
+    'transitionEnd' => '',
+    'transitionStart' => '',
+    'clickOutside' => true,
+    'position' => 'left',
+    'bp' => true
+])
+@php
+    $positionClasses = (object) [
+        'left' => (object) [
+            'position' => 'left-0',
+            'box' => 'justify-end',
+            'svg' => '',
+        ],
+        'right' => (object)[
+            'position' => 'right-0',
+            'box' => 'justify-start',
+            'svg' => 'rotate-180',
+        ],
+    ];
+    $con = ($sidebar == 'expand');
+    $sidebars = (object) [
+        'custom' => (object) [
+            'class' => "$class",
+            'transitionStart' => "$transitionStart",
+            'transitionEnd' => "$transitionEnd"
+        ],
+        'expand' => (object) [
+            'class' => "h-[calc(100vh-50px)] md:h-[calc(100vh-70px)] py-2 transition-[width] duration-500 $class",
+            'transitionStart' => 'w-16',
+            'transitionEnd' => 'w-52'
+        ],
+        'standard' => (object) [
+            'class' => "w-52 absolute top-0 md:relative h-[calc(100vh-50px)] md:h-[calc(100vh-70px)] py-2 transition-[width] duration-500 overflow-hidden $class ".$positionClasses->$position->position,
+            'transitionStart' => 'w-0',
+            'transitionEnd' => 'w-52'
+        ]
+    ];   
+@endphp
+@pushOnce('scripts')
+    <script>
+        function sideBarFn(id, state){
+            return {
+                sidebar: state,
+                id: id,
+                open(id)
+                {
+                    (this.id == id) ? this.sidebar = true : '';
+                },
+                close(id)
+                {
+                    (this.id == id) ? this.sidebar = false : '';
+                },
+                toggle(id)
+                {
+                    (this.id == id) ? this.sidebar = !this.sidebar : '';
+                },
+                getState()
+                {
+                    return this.sidebar;
+                },
+                getId()
+                {
+                    return this.id;
+                }
+            }
+        }
+    </script>
+@endPushOnce
+
+<section class="relative" x-data="sideBarFn('{{$id}}', {{ $state }})" >
+    
+    <nav class="{{ $sidebars->$sidebar->class }}"
+    id="{{ $id }}"
+    @if($bp)
+        x-init="window.innerWidth < 765 ? sidebar = false : ''"
+    @endif
+    @if($con)
+        :class="sidebar ? '{{ $sidebars->$sidebar->transitionEnd }}' : '{{ $sidebars->$sidebar->transitionStart }}'"
+    @else
+    x-cloak
+    x-show="sidebar"
+    x-transition:enter="transition-[width] duration-500"
+    x-transition:enter-start="{{ $sidebars->$sidebar->transitionStart }}"
+    x-transition:enter-end="{{ $sidebars->$sidebar->transitionEnd }}"
+    x-transition:leave="transition-[width] {{ $sidebars->$sidebar->transitionStart }} duration-500"
+    @endif
+    @if($clickOutside)
+    x-on:click.outside="close(id)"
+    @endif
+    x-on:open-sidebar.window="open($event.detail.id)"
+    x-on:close-sidebar.window="close($event.detail.id)"
+    x-on:toggle-sidebar.window="toggle($event.detail.id)"
+    {{ $attributes }}
+    >
+        @isset($brand)
+            {{ $brand }}
+        @endisset
+
+
+        @if($con)
+            <div class="flex {{ $positionClasses->$position->box }}">
+                @isset($icon)
+                    {{ $icon }}
+                @else
+                    <x-stm::button btn="icon" x-on:click="toggle(id)">
+                        <div class="transition-transform duration-300" :class="sidebar ? '[transform:rotateY(180deg)]' : ''">
+                            <svg class="size-6 {{$positionClasses->$position->svg}}" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" >
+                                <path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z" />
+                            </svg>
+                        </div>
+                    </x-stm::button>
+                @endisset
+            </div>
+        @endif
+
+        <div class="side overflow-y-auto overflow-x-hidden h-[400px]">
+            {{ $slot }}
+        </div>
+  
+    </nav>
+    <template x-teleport="head">
+        <style>
+            .side::-webkit-scrollbar {
+            width: 2px;
+            }
+            /* Track */
+            .side::-webkit-scrollbar-track {
+            background: transparent; 
+            }
+            
+            /* Handle */
+            .side::-webkit-scrollbar-thumb {
+            background: gray ;
+            }
+        
+            /* Handle on hover */
+            .side::-webkit-scrollbar-thumb:hover {
+            background: darkgray ;
+            width: 10px;
+            }
+        </style>
+    </template>
+</section>
+
+
+
